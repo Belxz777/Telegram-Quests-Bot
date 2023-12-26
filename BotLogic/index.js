8fe26b8208f977ce0c332ee4946aa7944f83c17c
Roman <140193303+Belxz777@users.noreply.github.com>
2023-12-26 15:38:23 +0400

    new whting

--- BotLogic/index.js
+++ BotLogic/index.js
@@ -1,25 +1,20 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+require('dotenv').config()
 const express = require('express')
 const app = express()
-//all
-const axios = require('axios')
-require('dotenv').config()
 const telegramBot = require('node-telegram-bot-api');
 const token = process.env.TOKEN
 console.log(token)
 const cors  =require('cors')
-
 app.use(express.json())
 app.use(cors())
+  const bot = new telegramBot(token, {polling:false});
+  const webhookUrl = 'https://bot-logic.vercel.app/bot6872431706:AAHaBvKWq2y1Ba1mNzJ1hGyBIyfMPdFe4o';
 
-  const bot = new telegramBot(token, {polling: true});
-  app.post(`/bot${token}`, (req, res) => {
-      bot.processUpdate(req.body);
-      res.sendStatus(200);
-    });
+  // Устанавливаем вебхук
+  
+
   bot.on('message',async  (msg) => {
     const chatId = msg.chat.id;
-    const receiver = msg.from.username
     const msgText = msg.text    
     if(msgText ==='/start'){
      await bot.sendMessage(chatId,"Привет ,это бот который будет давать задания на определенной локации  ")
@@ -34,7 +29,7 @@ app.use(cors())
               [{text:'Админ',url:'https://t.me/Belxz999'}]
           ]
         }}) 
-        await bot.sendContact(chatId,'8 982 124 9732','Roman Belxz')
+  
             }
       if(msgText ==='/location'){
         await bot.sendMessage(chatId,'Нажми на кнопку снизу <b> &#128071</b> ',{
@@ -60,13 +55,16 @@ app.use(cors())
     console.log(msg.location.latitude);
     console.log(msg.location.longitude);
   });
+  app.post(`/bot${token}`, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+  });
    const port = 4500;
 app.listen(port, () => {
   console.log(`Bot is running on ${port}`)
 })
-const webhookUrl = 'https://bot-logic.vercel.app/bot' + token;
 bot.setWebHook(webhookUrl).then(() => {
-  console.log(`Webhook set to: ${webhookUrl}`);
+  console.log('Вебхук успешно установлен');
 }).catch((error) => {
-  console.error('Error setting webhook:', error.message);
+  console.error('Ошибка при установке вебхука:', error);
 });
